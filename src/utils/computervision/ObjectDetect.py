b7801e2fafb6193fa6dbfe075993a87de83383b5
Paul Stanik <paulstanik3@gmail.com>
2022-05-05 20:30:46 -0700

    updated object detection

--- src/utils/computervision/ObjectDetect.py
+++ src/utils/computervision/ObjectDetect.py
@@ -1,248 +1,177 @@
-#python3 main.py --bin ssd_mobilenet/MobileNetSSD_deploy.bin --xml ssd_mobilenet/MobileNetSSD_deploy.xml --labels ssd_mobilenet/labels.txt --conf_threshold=0.4
-#python3 main.py --bin ssd_resnet50/saved_model.bin --xml ssd_resnet50/saved_model.xml --labels ssd_resnet50/labels.txt --conf_threshold=0.1
+# Copyright (c) 2019, Bosch Engineering Center Cluj and BFMC organizers
+# All rights reserved.
 
-#################################################
-# Original script could be found on 			#
-# https://github.com/leswright1977/RPI4_NCS2	#	
-# Put this script into 'RPI4_NCS2/src'			#
-#################################################
-import cv2
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+
+import socket
+import struct
 import time
-import numpy
-from multiprocessing import Process
+import numpy as np
+import json
+
+from threading       import Thread
+from threading       import Timer
+from multiprocessing import Pipe
 from multiprocessing import Queue
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-from sys import getsizeof
 
-import argparse
+import cv2
+import matplotlib.pyplot as plt
 
-#hacked from:
-#https://software.intel.com/articles/OpenVINO-Install-RaspberryPI
-#https://opencv2-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
-#https://github.com/PINTO0309/MobileNet-SSD-RealSense/blob/master/SingleStickSSDwithUSBCamera_OpenVINO_NCS2.py
-#https://raspberrypi.stackexchange.com/questions/87062/overhead-counter
+from src.templates.workerprocess import WorkerProcess
 
-#Les Wright Dec 24 2018 (modified to support picam 30 Dec 2018)
-#refined to warp speed (30 fps video, 28 fps inferencing 20 Feb 2019)
+#from pynput import keyboard 
+#from src.utils.tflite import ObjectDetector
+import sys
+#np.set_printoptions(threshold=sys.maxsize)
 
-#Note cv2.dnn.blobFromImage, the size is present in the XML files, we could write a preamble to go get that data,
-#Then we dont have to explicitly set it!
+    
+def nothing(x):
+    pass
 
-ap = argparse.ArgumentParser(description='NCS2 PiCamera')
-ap.add_argument('-b', '--bin')
-ap.add_argument('-x', '--xml')
-ap.add_argument('-l', '--labels')
-ap.add_argument('-pb', '--protobox')
-ap.add_argument('-pbtxt', '--protoboxtxt')
-ap.add_argument('-ct', '--conf_threshold', default=0.5)
-args = vars(ap.parse_args())
-# confThreshold = 0.4
-confThreshold = float(args['conf_threshold'])
+class ObjectDetection(WorkerProcess):
+    
+    # ===================================== INIT =========================================
+    def __init__(self, inPipes, outPipes):
+        """Process used for sending images over the network to a targeted IP via UDP protocol 
+        (no feedback required). The image is compressed before sending it. 
 
-# Load the model
-if not(args['bin']) is None:
-	print("[INFO] OpenVINO format")
-	# net = cv2.dnn.readNet('models/MobileNetSSD_deploy.xml', 'models/MobileNetSSD_deploy.bin')
-	net = cv2.dnn.readNet(args['xml'], args['bin'])
-elif not(args['protobox']) is None:
-	print("[INFO] Tensorflow format")
-	net = cv2.dnn.readNetFromTensorflow(args['protobox'], args['protoboxtxt'])
+        Used for visualizing your raspicam images on remote PC.
+        
+        Parameters
+        ----------
+        inPs : list(Pipe) 
+            List of input pipes, only the first pipe is used to transfer the captured frames. 
+        outPs : list(Pipe) 
+            List of output pipes (not used at the moment)
+        """
+        super(ObjectDetection,self).__init__(inPipes, outPipes)
+        self.HEIGHT = 300
+        self.WIDTH = 300
+        self.inPs = inPipes[0]
+        self.outPs = outPipes[0]
+        
+    # ===================================== RUN ==========================================
+    def run(self):
+        """Apply the initializing methods and start the threads.
+        """
+        #self._init_socket()
+        super(ObjectDetection,self).run()
 
-# Specify target device
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_MYRIAD)
+    # ===================================== INIT THREADS =================================
+    def _init_threads(self):
+        """Initialize the sending thread.
+        """
+        
+        if self._blocker.is_set():
+            return
+        #self.listener.daemon = self.daemon
+        #self.threads.append(self.listener)
+        
+        streamTh = Thread(name='ObjectDetectionThread',target = self._detect_objects, args= (self.inPs, self.outPs))
+        streamTh.daemon = True
+        self.threads.append(streamTh)
+    
+    
+    
+    # ===================================== SEND THREAD ==================================
+    def _detect_objects(self, inP, outPs):
+        """Sending the frames received thought the input pipe to remote client by using the created socket connection. 
+        
+        Parameters
+        ----------
+        inP : Pipe
+            Input pipe to read the frames from CameraProcess or CameraSpooferProcess. 
+        """
+        inputQueue = Queue(maxsize = 1)
+        outputQueue = Queue(maxsize = 1)
+        confThreshold = 0.5
 
-#Misc vars
-font = cv2.FONT_HERSHEY_SIMPLEX
-frameWidth = 304
-frameHeight = 304
-queuepulls = 0.0
-detections = 0
-fps = 0.0
-qfps = 0.0
+        # Load the model
+        xml_path = '/home/pi/repos/Brain/src/utils/openvino/ssd_mobilenet/bosch_model_0/saved_model.xml'
+        bin_path = '/home/pi/repos/Brain/src/utils/openvino/ssd_mobilenet/bosch_model_0/saved_model.bin'
+        labels_file = '/home/pi/repos/Brain/src/utils/openvino/labels.txt'
+        net = cv2.dnn.readNet(xml_path, bin_path)
 
+        # Specify target device
+        net.setPreferableTarget(cv2.dnn.DNN_TARGET_MYRIAD)
 
+        with open(labels_file, 'r') as f:
+            labels = [x.strip() for x in f]
+        print(labels)
 
-#initialize the camera and grab a reference to the raw camera capture
-#well this is interesting, we can closely match the input of the network!
-#this 'seems' to have improved accuracy!
-camera = PiCamera()
-camera.resolution = (304,304)
-camera.framerate = 35
-rawCapture = PiRGBArray(camera, size=(304, 304)) 
+        
+        while True:
+            try:
+                if not inputQueue.empty():
+                    frame = inputQueue.get()
+                    resframe = cv2.resize(frame, (300, 300))
+                    blob = cv2.dnn.blobFromImage(resframe, 1, size=(300, 300), mean=(127.5,127.5,127.5), swapRB=True, crop=False)
+                    net.setInput(blob)
+                    out = net.forward()
+                    data_out = []
 
-# allow the camera to warmup
-time.sleep(0.1)
+                    for detection in out.reshape(-1, 7):
+                        inference = []
+                        obj_type = int(detection[1]-1)
+                        confidence = float(detection[2])
+                        xmin = int(detection[3] * frame.shape[1])
+                        ymin = int(detection[4] * frame.shape[0])
+                        xmax = int(detection[5] * frame.shape[1])
+                        ymax = int(detection[6] * frame.shape[0])
 
+                    if confidence > 0: #ignore garbage
+                        inference.extend((obj_type,confidence,xmin,ymin,xmax,ymax))
+                        data_out.append(inference)
 
-# labels_file = 'models/labels.txt'
-labels_file = args['labels']
-with open(labels_file, 'r') as f:
-	labels = [x.strip() for x in f]
-print(labels)
+                    outputQueue.put(data_out)
+                else:
+                    stamps, image = inP.recv()
+                    inputQueue.put(image.array)
 
+                
+                if not outputQueue.empty():
+                    out = outputQueue.get()
+                    '''
+                        output the detections
+                    '''
+                    for outP in outPs:
+                        outP.send(out)
 
-#define the function that handles our processing thread
-def classify_frame(net, inputQueue, outputQueue):
-# keep looping
-	while True:
-		# check to see if there is a frame in our input queue
-		if not inputQueue.empty():
-			# grab the frame from the input queue, resize it, and
-			# construct a blob from it
-			frame = inputQueue.get()
-			resframe = cv2.resize(frame, (300, 300))
-			blob = cv2.dnn.blobFromImage(resframe, 1, size=(300, 300), mean=(127.5,127.5,127.5), swapRB=True, crop=False)
-			net.setInput(blob)
-			out = net.forward()
-
-			data_out = []
-
-			for detection in out.reshape(-1, 7):
-				inference = []
-				obj_type = int(detection[1]-1)
-				confidence = float(detection[2])
-				xmin = int(detection[3] * frame.shape[1])
-				ymin = int(detection[4] * frame.shape[0])
-				xmax = int(detection[5] * frame.shape[1])
-				ymax = int(detection[6] * frame.shape[0])
-
-				if confidence > 0: #ignore garbage
-					inference.extend((obj_type,confidence,xmin,ymin,xmax,ymax))
-					data_out.append(inference)
-
-			outputQueue.put(data_out)
-			
-
-# initialize the input queue (frames), output queue (out),
-# and the list of actual detections returned by the child process
-inputQueue = Queue(maxsize=1)
-outputQueue = Queue(maxsize=1)
-out = None
-
-# construct a child process *indepedent* from our main process of
-# execution
-print("[INFO] starting process...")
-p = Process(target=classify_frame, args=(net,inputQueue,outputQueue,))
-p.daemon = True
-p.start()
-
-print("[INFO] starting capture...")
-
-#time the frame rate....
-timer1 = time.time()
-frames = 0
-queuepulls = 0
-timer2 = 0
-t2secs = 0
-
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	if queuepulls ==1:
-		timer2 = time.time()
-	# Capture frame-by-frame
-	frame = frame.array
-
-	# if the input queue *is* empty, give the current frame to
-	# classify
-	if inputQueue.empty():
-		inputQueue.put(frame)
-
-	# if the output queue *is not* empty, grab the detections
-	if not outputQueue.empty():
-		out = outputQueue.get()
-		queuepulls += 1
-		#print(len(out))
-		#print(getsizeof(out))
-
-
-	# check to see if 'out' is not empty
-	if out is not None:
-		# loop over the detections
-		for detection in out:
-			#print(detection)
-			#print("\n")
-			
-			objID = detection[0]
-			confidence = detection[1]
-
-			xmin = detection[2]
-			ymin = detection[3]
-			xmax = detection[4]
-			ymax = detection[5]
-
-			if confidence > confThreshold:
-				#bounding box
-				cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color=(0, 255, 255))
-
-				#label
-				cv2.rectangle(frame, (xmin-1, ymin-1),\
-				(xmin+70, ymin-10), (0,255,255), -1)
-				#labeltext
-				cv2.putText(frame,' '+labels[objID]+' '+str(round(confidence,2)),\
-				(xmin,ymin-2), font, 0.3,(0,0,0),1,cv2.LINE_AA)
-				detections +=1 #positive detections
-	
-
-
-	# Display the resulting frame
-
-	cv2.rectangle(frame, (0, 0),\
-	(90, 15), (0,0,0), -1)
-
-	cv2.putText(frame,'Threshold: '+str(round(confThreshold,1)), (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0, 255, 255), 1, cv2.LINE_AA)
-
-
-	cv2.rectangle(frame, (220, 0),\
-	(300, 25), (0,0,0), -1)
-	cv2.putText(frame,'VID FPS: '+str(fps), (225, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0, 255, 255), 1, cv2.LINE_AA)
-
-	cv2.putText(frame,'NCS FPS: '+str(qfps), (225, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0, 255, 255), 1, cv2.LINE_AA)
-
-	cv2.rectangle(frame, (0, 265),\
-	(170, 300), (0,0,0), -1)
-	cv2.putText(frame,'Positive detections: '+str(detections), (10, 280), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0, 255, 255), 1, cv2.LINE_AA)
-
-	cv2.putText(frame,'Elapsed time: '+str(round(t2secs,2)), (10, 290), cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0, 255, 255), 1, cv2.LINE_AA)
-
-	cv2.namedWindow('frame',cv2.WINDOW_NORMAL)
-	cv2.resizeWindow('frame',frameWidth,frameHeight)
-	cv2.imshow('frame',frame)
-	
-	# FPS calculation
-	frames += 1
-	if frames >= 1:
-		end1 = time.time()
-		t1secs = end1-timer1
-		fps = round(frames/t1secs,2)
-	if queuepulls > 1:
-		end2 = time.time()
-		t2secs = end2-timer2
-		qfps = round(queuepulls/t2secs,2)
+                stamps, image = inP.recv()
+                rgb_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 
 
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
-	
-	keyPress = cv2.waitKey(1)
-	
-	if keyPress == 113:
-		break
-
-	if keyPress == 82:
-		confThreshold += 0.1
-
-	if keyPress == 84:
-		confThreshold -= 0.1
-
-	if confThreshold >1:
-		confThreshold = 1
-	if confThreshold <0:
-		confThreshold = 0
-
-
-
-
-
-cv2.destroyAllWindows()
-
+                
+            except Exception as e:
+                print("CameraStreamer failed to stream images:",e,"\n")
+                # Reinitialize the socket for reconnecting to client.  
+                #self.connection = None
+                #self._init_socket()
+                pass
+        
+        
+    

97603757a106af1704d5bd362e28647fff314e05
Paul Stanik <paulstanik3@gmail.com>
2022-05-06 20:49:37 -0700

    testing only sending speed change once

--- src/hardware/serialhandler/writethread.py
+++ src/hardware/serialhandler/writethread.py
@@ -57,7 +57,6 @@ class WriteThread(Thread):
             command = self.inP.recv()
             # Unpacking the dictionary into action and values
             command_msg = self.messageConverter.get_command(**command)
-            for ii in range(0, 50):
-                self.serialCom.write(command_msg.encode('ascii'))
-                self.logFile.write(command_msg)
+            self.serialCom.write(command_msg.encode('ascii'))
+            self.logFile.write(command_msg)
 

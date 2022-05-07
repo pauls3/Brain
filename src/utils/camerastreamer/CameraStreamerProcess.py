28e00af8d0ec3856d32d075d73c70107d40bc01e
Paul Stanik <paulstanik3@gmail.com>
2022-05-06 23:43:02 -0700

    setting max line length to 200 by default

--- src/utils/camerastreamer/CameraStreamerProcess.py
+++ src/utils/camerastreamer/CameraStreamerProcess.py
@@ -223,7 +223,7 @@ class CameraStreamerProcess(WorkerProcess):
                 
                 # get lines
                 #lines = cv2.HoughLinesP(edges, 1, np.pi/180, 25, maxLineGap=200)
-                lines = cv2.HoughLinesP(edges, 1, np.pi/hough0, hough1)
+                lines = cv2.HoughLinesP(edges, 1, np.pi/hough0, hough1, maxLineGap=200)
                 
                 if lines is not None:
                     for jj in range(0, len(lines)):

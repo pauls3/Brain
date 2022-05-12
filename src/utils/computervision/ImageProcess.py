a703b2fd4efb87374beaf55447bf988087f17c30
Paul Stanik <paulstanik3@gmail.com>
2022-05-12 12:08:22 +0300

    switching directions of roundabout

--- src/utils/computervision/ImageProcess.py
+++ src/utils/computervision/ImageProcess.py
@@ -699,11 +699,11 @@ class ImageProcess(WorkerProcess):
             passed_time = timer2 - timer1
 
             if passed_time > 1.5 and steerFlag == 0:
-                self._test_steering(-0.8)
+                self._test_steering(0.8)
                 steerFlag = 1
 
             if timer2 - timer1 > 10 and steerFlag == 1:
-                self._test_steering(0.75)
+                self._test_steering(-0.75)
                 steerFlag = 2
 
             if timer2 - timer1 > 13 and steerFlag == 2:

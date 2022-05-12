4ea7c7785cebe6fa3d2411bcc2bcc38331b73c6a
Paul Stanik <paulstanik3@gmail.com>
2022-05-12 09:49:25 +0300

    adjusting time

--- src/utils/computervision/ImageProcess.py
+++ src/utils/computervision/ImageProcess.py
@@ -705,7 +705,7 @@ class ImageProcess(WorkerProcess):
                 self._test_steering(0.75)
                 steerFlag = 2
 
-            if timer2 - timer1 > 15 and steerFlag == 2:
+            if timer2 - timer1 > 13 and steerFlag == 2:
                 self._test_steering(0.1)
                 steerFlag = 3
             

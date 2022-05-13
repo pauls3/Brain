8f8d8521419fec26b58fedbee0af5eb33eb8d7e6
Paul Stanik <paulstanik3@gmail.com>
2022-05-13 09:37:44 +0300

    testing speed lane keeping

--- src/utils/computervision/ImageProcess.py
+++ src/utils/computervision/ImageProcess.py
@@ -442,8 +442,6 @@ class ImageProcess(WorkerProcess):
 
     def _lane_keeping(self, image):
 
-        print('lane keeping')
-
         winname = 'RebelDynamics'
         cv2.namedWindow(winname)
         #cv2.moveWindow(winname, 0,0)
@@ -495,11 +493,11 @@ class ImageProcess(WorkerProcess):
         # convert to grayscale
         #gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         # crop with mask
-        #img_crop_gray = cv2.bitwise_and(gray_img, gray_img, mask=stencil)
-        # img_crop = cv2.bitwise_and(image, image, mask=stencil)
+        # img_crop_gray = cv2.bitwise_and(gray_img, gray_img, mask=stencil)
+        img_crop = cv2.bitwise_and(image, image, mask=stencil)
         # convert to grayscale
-        # img_crop_gray = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
-        img_crop_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        img_crop_gray = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
+        # img_crop_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         # blur
         #blur_img = cv2.blur(img_crop_gray, (10,10))
         blur_img = cv2.GaussianBlur(img_crop_gray, (5,5), 0)
